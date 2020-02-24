efb6ef4f65604efc18b96694510f37529031dd06
kei hasegawa <kei.hasegawa@gmail.com>
2020-02-25 06:20:11 +0900

    FalconScreenshotTestでディレクトリ処理を@Beforeで行う

--- app/src/androidTest/java/red/torch/vrtsample/activity/FalconScreenshotTest.kt
+++ app/src/androidTest/java/red/torch/vrtsample/activity/FalconScreenshotTest.kt
@@ -4,6 +4,7 @@ import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.GrantPermissionRule
 import com.jraska.falcon.Falcon
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -21,15 +22,20 @@ class FalconScreenshotTest {
         android.Manifest.permission.WRITE_EXTERNAL_STORAGE
     )
 
-    @Test
-    fun simpleScreenshotTest() {
-        activityScenarioRule.scenario.onActivity {
-            val directory = File("/sdcard", "screenshots")
-            if (!directory.exists()) {
-                if (!directory.mkdirs()) {
-                    throw IllegalAccessException("Unable to create screenshot directory " + directory.absolutePath)
-                }
+    private val directory = File("/sdcard", "screenshots")
+
+    @Before
+    fun setUp() {
+        if (!directory.exists()) {
+            if (!directory.mkdirs()) {
+                throw IllegalAccessException("Unable to create screenshot directory " + directory.absolutePath)
             }
+        }
+    }
+
+    @Test
+    fun falconScreenshotTest() {
+        activityScenarioRule.scenario.onActivity {
             // Saving screenshot to file
             Falcon.takeScreenshot(it, File(directory, "falcon_simple.png"))
 
